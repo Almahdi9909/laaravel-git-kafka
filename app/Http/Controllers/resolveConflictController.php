11ccd78b19915e48c72562cb9420b79cae2f9bdf
Almahdi9909 <=>
2022-02-24 01:56:49 +0300

    reslove conflict

---
+++ app/Http/Controllers/resolveConflictController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class resolveConflictController extends Controller
+{
+    function retrive(){
+        return view('hello');
+    }
+}
