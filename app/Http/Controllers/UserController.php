9f50f7ceb618e26a1bf6a0feb15b270f65a6114a
Almahdi9909 <=>
2022-02-24 00:20:45 +0300

    credintials

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    
+
+    function addUser(){
+        return [
+            "username" => "almahdi",
+            "password" => "jassim"
+        ];
+    }
 }
